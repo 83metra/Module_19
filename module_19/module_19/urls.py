8d18756aa2512e2268aac31c7cd7fff9273d457d
83metra <jumo_004@mail.ru>
2024-11-26 19:04:30 +0200

    Модуль 19, задание 3. Домашнее задание по теме "MVT. Вывод объектов в шаблоны". .
    Задача "Время объединять"

--- module_19/module_19/urls.py
+++ module_19/module_19/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from task1.views import platform, shop, cart, sign_up_by_django, sign_up_by_html
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', platform, name='platform'),
+    path('shop/', shop, name='shop'),
+    path('cart/', cart, name='cart'),
+    path('django_sign_up/', sign_up_by_django),
+    path('sign/', sign_up_by_html)
 ]
--- module_19/task1/views.py
+++ module_19/task1/views.py
@@ -1,3 +1,88 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .forms import UserRegister
 
 # Create your views here.
+def platform(request):
+    title = 'Магазин ретро-игр'
+    text = 'Главная страница'
+    context = {
+        'title': title,
+        'text': text
+    }
+    return render(request, 'fourth_task/platform.html', context)
+
+def shop(request):
+    title = 'Магазин'
+    text = 'Магазин'
+    context = {
+        'title': title,
+        'text': text,
+        #'games:': ['Doom II', 'Warcraft', 'Vikings'],
+        'games': ['Doom II', 'Warcraft', 'Vikings']
+    }
+    return render(request, 'fourth_task/shop.html', context)
+
+def cart(request):
+    title = 'Корзина'
+    text = 'Корзина пуста'
+    context = {
+        'title': title,
+        'text': text
+    }
+    return render(request, 'fourth_task/cart.html', context)
+
+
+def sign_up_by_django(request):
+    users = ['Otto', 'Moritz', 'Ruslan', 'Finduss', 'Karl']
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)  # обращение request.POST не забыть
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = int(form.cleaned_data['age'])
+
+
+            if password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+                # info.update({'error': 'Пользователь уже существует.'})
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif username in users:
+                info['error'] = 'Пользователь уже существует'
+            else:
+                return render(request, 'fifth_task/registration_page.html', info)
+
+
+    else:
+        info = {
+            'form': UserRegister()
+        }
+
+    return render(request, 'fifth_task/registration_page.html', info)  # передача словаря без переменной не работает
+    # return render(request, 'registration_page.html', {'form': form}) # не работает
+
+
+def sign_up_by_html(request):
+    users = ['Otto', 'Moritz', 'Ruslan', 'Finduss', 'Karl']
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if username in users:
+            info['error'] = 'Пользователь уже существует.'
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают.'
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18.'
+        else:
+            info['information'] = 'Приветствуем, %s!' % username
+            return render(request, 'registration_page.html', info)
+            # return HttpResponse('Приветствуем, %s!' % username)
+
+    return render(request, 'fifth_task/registration_page.html', info)
